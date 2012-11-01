614f3943819dc9f4054ec8b0d77fa19097c5a50f
Jeremy Day <jeremyday@Jeremys-MacBook-Pro.local>
2012-11-01 18:20:24 +1000

    Alerts for mandatory and optional updates. Block via subview.

---
+++ Example/ManUpDemo/ManUpDemo/DemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  DemoViewController.h
+//  ManUpDemo
+//
+//  Created by Jeremy Day on 1/11/12.
+//  Copyright (c) 2012 Burleigh Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoViewController : UIViewController
+
+@end
